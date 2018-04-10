1325aa9a9f4041e3948c346b5748a89968989e87
顾新培培子 <roger_gu@sinobasedm.com>
2018-04-10 11:48:13 +0800

    RedisRegister

---
+++ Implement/RedisImplements/RedisEventRegister.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: Roog
+ * Date: 2018/4/5/005
+ * Time: 21:45
+ */
+
+namespace EventStatistics\Implement\RedisImplement;
+
+use EventStatistics\Abstracts\EventRegisterAbstract;
+
+class RedisEventRegister extends EventRegisterAbstract {
+
+
+}
